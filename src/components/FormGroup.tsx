90f86cd8f2bead88261b02c6e21425916c01f7b0
Estevan Jantsk <estevan.jantsk@gmail.com>
2023-06-29 15:09:43 +0300

    add simple components

---
+++ src/components/FormGroup.tsx
@@ -0,0 +1,10 @@
+import { cx } from "~/utils/cx";
+
+export function FormGroup(props: React.ComponentPropsWithoutRef<"div">) {
+
+  return (
+    <div {...props} className={cx("flex flex-col gap-1", props.className)}>
+      {props.children}
+    </div>
+  );
+}
